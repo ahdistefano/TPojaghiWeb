ed13a78260af861c90c4a29c6c5e8bf2b124dd00
ahdistefano <ahdistefano@gmail.com>
2023-12-24 00:39:12 -0300

    Set host and port params

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
 
 
 # pybabel extract -F babel.cfg -o messages.pot .
